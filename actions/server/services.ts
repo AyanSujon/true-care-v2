651c71a796edd30814e07379de207d6f54095a19
Ayan <sujon.seo.bd@gmail.com>
2026-01-18 17:23:34 +0600

    fixing error form this route: ./actions/server/services.ts:40:24

--- actions/server/services.ts
+++ actions/server/services.ts
@@ -28,16 +28,89 @@
 
 
 
+// "use server";
+
+// import { collections, dbConnect } from "@/lib/dbConnect";
+// import { ObjectId } from "mongodb";
+
+// export const getProducts = async () => {
+//   const servicesCollection = await dbConnect(collections.SERVICES)
+//   const services = await servicesCollection.find().toArray();
+//   // Convert _id to string for safe JSON serialization
+//   return services.map((service) => ({
+//     ...service,
+//     _id: service._id.toString(),
+//   }));
+// };
+
+// export const getSingleProduct = async (id: string) => {
+//   // Validate ID properly
+//   if (!id || !ObjectId.isValid(id)) {
+//     return {};
+//   }
+
+//   const query = { _id: new ObjectId(id) };
+//   const servicesCollection = await dbConnect(collections.SERVICES)
+//   const service = await servicesCollection.findOne(query);
+
+//   // Safely handle not found
+//   if (!service) {
+//     return {};
+//   }
+
+//   return {
+//     ...service,
+//     _id: service._id.toString(),
+//   };
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 "use server";
 
 import { collections, dbConnect } from "@/lib/dbConnect";
 import { ObjectId } from "mongodb";
 
+
+// Define the TypeScript type for a service document
+export interface Service {
+  _id: ObjectId;
+  name: string;
+  price: number;
+  description?: string;
+
+}
+
+
 export const getProducts = async () => {
   const servicesCollection = await dbConnect(collections.SERVICES)
   const services = await servicesCollection.find().toArray();
   // Convert _id to string for safe JSON serialization
-  return services.map((service) => ({
+  return services.map((service: Service) => ({
     ...service,
     _id: service._id.toString(),
   }));
